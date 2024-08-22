db69d37ce6bf33319e5139ad3a895186fefbb8b2
Monika-Dangar <dangarmonika191023@gmail.com>
2024-08-22 21:34:03 +0530

    Solved CORS by adding frontend URL in Server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,6 +9,13 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// CORS configuration
+app.use(cors({
+  origin: 'https://portfoliosite-hazel.vercel.app', // Allow your frontend domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true // if you need to allow cookies or headers
+}));
+
 app.use(cors());
 app.use(express.json());
 
