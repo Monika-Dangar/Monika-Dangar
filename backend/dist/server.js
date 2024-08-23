b19ecf388fa79146dbfe44777baff73370141d06
Monika-Dangar <dangarmonika191023@gmail.com>
2024-08-23 11:44:54 +0530

    minor changes

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
@@ -13,19 +15,26 @@ const app = (0, express_1.default)();
 const port = process.env.PORT || 5000;
 app.use(express_1.default.json());
 // CORS configuration
-app.use((0, cors_1.default)({
-    origin: 'https://monika-dangar-portfoliosite.vercel.app/', // Allow your frontend domain
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-}));
+app.use(
+  (0, cors_1.default)({
+    origin: "https://monika-dangar-portfoliosite.vercel.app", // Allow your frontend domain
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 const uri = process.env.ATLAS_URI;
 if (!uri) {
-    throw new Error('MongoDB connection string is not defined in environment variables');
+  throw new Error(
+    "MongoDB connection string is not defined in environment variables"
+  );
 }
-mongoose_1.default.connect(uri)
-    .then(() => console.log("MongoDB database connection established successfully"))
-    .catch(err => console.error('MongoDB connection error:', err));
-app.use('/fellow', fellow_1.default);
+mongoose_1.default
+  .connect(uri)
+  .then(() =>
+    console.log("MongoDB database connection established successfully")
+  )
+  .catch((err) => console.error("MongoDB connection error:", err));
+app.use("/fellow", fellow_1.default);
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
